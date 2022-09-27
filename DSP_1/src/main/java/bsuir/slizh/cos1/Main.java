f3fc4b79c59786ff799219611d3c5dae7da8fc0b
Anton Slizh <antonslizh@gmail.com>
2022-09-28 01:31:05 +0300

    add amplitude modulation

--- DSP_1/src/main/java/bsuir/slizh/cos1/Main.java
+++ DSP_1/src/main/java/bsuir/slizh/cos1/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             int sampleRate = 44100;        // Samples per second
-            double duration = 3.0;        // Seconds
+            double duration = 1.0;        // Seconds
 
             // Calculate the number of frames required for specified duration
             long numFrames = (long) (duration * sampleRate);
@@ -27,20 +27,38 @@ public class Main {
                 int toWrite = (remaining > 100) ? 100 : (int) remaining;
 
                 double dc = 0.5;
-                double f = 2048;
+                double f = 512;
                 double T = 1 / f;
-                double A = 5;
+                double A = 1;
+
+                double Fm = 512;
+                double Fc = 44100;
+                double Am = 1;
+                double Ac = 0.5;
+                double m = 0.5;
 
                 // Fill the buffer, one tone per channel
                 for (int s = 0; s < toWrite; s++, frameCounter++) {
+
+//                    double Um = Am * Math.sin(2.0 * Math.PI * Fm * frameCounter / sampleRate);
+//                    double Uc = Ac * Math.sin(2.0 * Math.PI * Fc * frameCounter / sampleRate + Math.PI / 2);
+//                    double Uam = Uc * (1.0 + m * Um / 1.0);
+//
+//                    buffer[0][s] = Uam;
+
                     // Sin wave
-                    //buffer[0][s] = A * Math.sin(2.0 * Math.PI * f * frameCounter / sampleRate);
+                    buffer[0][s] = A * Math.sin(2.0 * Math.PI  * f * frameCounter / sampleRate);
+
                     // Square wave
                     //buffer[0][s] = ((((double) frameCounter / sampleRate) % T) / T) < dc ? A : -A;
+                    //buffer[0][s] = ((2.0 * Math.PI * f * frameCounter / sampleRate) % (2 * Math.PI)) / (2 * Math.PI) < dc ? A : -A;
+
                     // Triangle wave
                     //buffer[0][s] = 2 * A / Math.PI / Math.sin(Math.sin(2 * Math.PI * f * frameCounter / sampleRate));
+
                     // Sawtooth wave
                     //buffer[0][s] = -2 * A / Math.PI / Math.tan(1 / Math.tan(Math.PI * f * frameCounter / sampleRate));
+
                     // Random wave
                     //buffer[0][s] = A * Math.random() * 2 - 1;
                 }
